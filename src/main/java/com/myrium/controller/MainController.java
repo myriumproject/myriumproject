c5fdbd8ee3386dc4d3f4eea7d2855ab76ff0b3f6
notuna87 <notuna74@gmail.com>
2025-07-21 16:50:13 +0900

    .gitignore setting

--- src/main/java/com/myrium/controller/MainController.java
+++ src/main/java/com/myrium/controller/MainController.java
@@ -1,5 +1,15 @@
 package com.myrium.controller;
 
-public class MainController {
+import java.util.Locale;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class MainController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "home";
+	}
 }
