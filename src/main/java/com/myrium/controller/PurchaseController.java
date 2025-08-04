2f6269488ca8ab96e98832add27893fa06fb909f
notuna87 <notuna74@gmail.com>
2025-08-05 01:49:03 +0900

    충돌 및 오류 수정 커밋

--- src/main/java/com/myrium/controller/PurchaseController.java
+++ src/main/java/com/myrium/controller/PurchaseController.java
@@ -94,11 +94,7 @@ public class PurchaseController {
 		orders.setCustomerId(customerName);
 		orders.setZipcode(zipcode);
 		orders.setAddress(addr);
-<<<<<<< HEAD
-		orders.setMessage(messageSelect);
-=======
 		orders.setDeliveryMsg(messageSelect);
->>>>>>> main
 		orders.setPhoneNumber(phone);
 		orders.setPayment(payment);
 		
--- src/main/java/com/myrium/domain/OrderDTO.java
+++ src/main/java/com/myrium/domain/OrderDTO.java
@@ -26,13 +26,7 @@ public class OrderDTO {
 	    private String deliveryMsg; // 상품요청사항
 	    private String payment; // 결제
 	    private int productId;
-<<<<<<< HEAD
-      private String orderDisplayId;
-	}
-=======
 	    private String orderDisplayId;
->>>>>>> main
-
 
     public void setOrderDate(String orderDate) {
         this.orderDate = orderDate;
